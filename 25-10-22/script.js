035f2e08370574e903abae4d71a72371a515527d
marcorivera92 <marco.rivera92@gmail.com>
2022-10-25 18:04:43 +0200

    Esercitazione 25-10-2022

---
+++ 25-10-22/script.js
@@ -0,0 +1,31 @@
+function addizione() {
+  let a = 1.0 * document.getElementById("a").value;
+  let b = 1.0 * document.getElementById("b").value;
+  document.getElementById("risultato").value = a + b;
+
+  console.log("risultato addizione: ", (value = a + b));
+}
+
+function sottrazione() {
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  document.getElementById("risultato").value = a - b;
+
+  console.log("risultato sottrazione: ", (value = a - b));
+}
+
+function moltiplicazione() {
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  document.getElementById("risultato").value = a * b;
+
+  console.log("risultato moltiplicazione: ", (value = a * b));
+}
+
+function divisione() {
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  document.getElementById("risultato").value = a / b;
+
+  console.log("risultato divisione: ", (value = a / b));
+}
