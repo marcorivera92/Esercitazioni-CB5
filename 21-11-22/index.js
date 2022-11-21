ab68c9b1c4ce9bcc6bfcd3060dd7259fe06def17
marcorivera92 <marco.rivera92@gmail.com>
2022-11-21 18:14:16 +0100

    FIX: Esercitazione 21-11-2022

--- 21-11-22/index.js
+++ 21-11-22/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const argvs = process.argv;
+// ESERCIZIO 1 - 2
+/* const argvs = process.argv;
 const argv = argvs.slice(2);
 const operation = argv[0];
 const num1 = parseInt(argv[1]);
@@ -20,4 +21,41 @@ if (!isNaN(num1) && isNaN(num2)) {
   } else {
     console.log("Result: " + num1 / num2);
   }
+} */
+
+// ESERCIZIO 3 with Switch
+const argvs = process.argv;
+const argv = argvs.slice(3);
+const operation = argvs[2];
+const num1 = parseInt(argv[1]);
+const num2 = parseInt(argv[2]);
+
+switch (operation) {
+  case "sum":
+    let sum = 0;
+    for (let i = 0; i < argv.length; i++) {
+      sum = sum + parseInt(argv[i]);
+    }
+
+    console.log("Result: " + sum);
+    break;
+
+  case "subtract":
+    console.log("Result: " + (num1 - num2));
+    break;
+
+  case "multiply":
+    let multi = 1;
+    for (let i = 0; i < argv.length; i++) {
+      multi = multi * parseInt(argv[i]);
+    }
+    console.log("Result: " + multi);
+    break;
+
+  case "div":
+    if (num2 === 0) {
+      console.log("Error.. not valid");
+      break;
+    } else console.log("Result: " + num1 / num2);
+    break;
 }
